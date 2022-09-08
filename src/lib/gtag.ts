288a161de23a70979df9e7b03b9d2b51667030de
Rohidul Islam <rohidul209@gmail.com>
2022-09-08 18:48:25 +0600

    debug

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -3,6 +3,8 @@ export const GA_TRACKING_ID = "G-989F7PZJ09";
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
   if (typeof window === "undefined") return;
+  console.log("page-view", url);
+
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
   });
@@ -16,8 +18,10 @@ type GTagEvent = {
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value }: GTagEvent): void => {
+export const event = (gTagEvent: GTagEvent): void => {
+  const { action, category, label, value } = gTagEvent;
   if (typeof window === "undefined") return;
+  console.log("g-tag-event", gTagEvent);
   window.gtag("event", action, {
     event_category: category,
     event_label: label,
