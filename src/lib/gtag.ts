1285a8ca671815e44179f32e675883fce2198a93
Rohidul Islam <rohidul209@gmail.com>
2022-09-08 18:36:32 +0600

    prod check removed

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,10 +1,7 @@
 export const GA_TRACKING_ID = "G-989F7PZJ09";
 
-const isProduction = process.env.NODE_ENV === "production";
-
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
-  if (!isProduction) return;
   if (typeof window === "undefined") return;
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
@@ -20,7 +17,6 @@ type GTagEvent = {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent): void => {
-  if (!isProduction) return;
   if (typeof window === "undefined") return;
   window.gtag("event", action, {
     event_category: category,
